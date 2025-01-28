fcecf07dcd9e1f863eb20858d616937f4f275800
David Pérez <deividpa022@gmail.com>
2025-01-28 11:45:04 -0500

    feat: wrap RootLayout with AuthProvider and add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,27 @@
 import "./globals.css";
+import { getServerSession } from "next-auth/next"
+import AuthProvider from "@/components/AuthProvider"
+
+import Header from '@/components/Header'
 import { Toaster } from "@/components/ui/toaster";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await getServerSession()
+
   return (
     <html lang="en">
       <body
         className="antialiased"
       >
-        {children}
+        <AuthProvider session={session}>
+          <Header />
+          {children}
+        </AuthProvider>
         <Toaster />
       </body>
     </html>
